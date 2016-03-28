381968de138fc91b23c5ace3dfcc45cb334702f0
Jared Greene <jaredgreene1@gmail.com>
2016-03-28 02:01:49 -0400

    Socket works. Now dealing with adjusting the protocal so that communication really works

--- Server.java
+++ Server.java
@@ -7,7 +7,7 @@ public class Server {
   	public static void main(String[] args){
     
 //Load Database   
-	if(args.size() != 1)
+	if(args.length != 1)
 	{
 		System.out.print("Usage: Server <port_number>");
 		System.exit(1);
@@ -19,7 +19,7 @@ public class Server {
        	HashMap<String, String> loginMap = new HashMap<String, String>();
        	String pwd;
        	String uName = "test";
-       	lisPort = Integer.parseInt(args[0]);
+       	int lisPort = Integer.parseInt(args[0]);
        	
        	while((pwd = loginText.readLine()) != null)
 		{
@@ -41,12 +41,14 @@ public class Server {
 		PrintWriter out =
 				new PrintWriter(clSock.getOutputStream(), true);
 		BufferedReader in = new BufferedReader(
-			new InputStreamReader(clientSocket.getInputStream()));
+			new InputStreamReader(clSock.getInputStream()));
 		
 		String textIn, textOut;
 //Ask for username and password (should spawn thread around here)
-		out.print("Hey! You've connected to the chat server. " +
-		"Please enter your username and password as prompted" + \n + "Password: ");
+		out.println("Hey! You've connected to the chat server. " +
+		"Please enter your username and password as prompted");
+	    System.out.println("bfr Password prompt");
+		out.print("Password: ");
 		textIn = in.readLine();
 		
 		out.println("This is the username you enetered, ya? - " + textIn);
